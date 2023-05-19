ea1e8e85a53cfb6accb63d2117600d5591f33d60
Brandon Sady <71144714+bsady17@users.noreply.github.com>
2023-05-19 13:21:25 -0600

    Update run.py
    Removed debug=True in run.py

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from app import app
 if __name__ == "__main__":
     url = "http://127.0.0.1:5000"
     webbrowser.open(url)
-    app.run(debug=True)
+    app.run(debug=False)
+    # app.run(debug=True) will cause the app to reload and relaunch every time changes are made
